4c57d966e5b099157833b4981322dbf8f41136c6
Victor Krieg <kriegv@uwplatt.edu>
2020-10-06 19:34:54 -0500

    Source Code
    Created Source Code for Example Program.

---
+++ Lab2Example/Test_Lab2Example/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Lab2Example
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
